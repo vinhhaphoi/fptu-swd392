eb65daccb0f641b866c101b7529fae0538166d03
vinhhaphoi <vinhhaphoi@users.noreply.github.com>
2026-01-26 11:51:48 +0700

    Security: move firebase config to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYrmh4fw-qlxtlWWeEhw8AXJyLDus4wHM",
-    authDomain: "vinhhaphoi-swd392.firebaseapp.com",
-    projectId: "vinhhaphoi-swd392",
-    storageBucket: "vinhhaphoi-swd392.firebasestorage.app",
-    messagingSenderId: "859215783127",
-    appId: "1:859215783127:web:e8e6fac3aee086f47a3c92",
-    measurementId: "G-L3F9667FYL"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
